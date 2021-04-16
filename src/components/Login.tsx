a0db90f30b928d87bc96c3af83a54f11b662af03
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-04-16 16:59:11 +0100

    login functionality added
    -need to do registering now

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,55 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { FcGoogle } from "react-icons/fc";
+import { AiOutlineLogin, AiOutlineUserAdd } from "react-icons/ai";
 
 function Login() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [register, setRegister] = useState<boolean>(false);
+  const [validated, setValidated] = useState<boolean>(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation(); //STOPS CODE HERE
+    } else {
+      loginUser();
+      //LOGIN FUNC
+    }
+    setValidated(true);
+  };
+
+  const loginUser = async () => {
+    try {
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_URL}/users/login`,
+        {
+          method: "POST",
+          body: JSON.stringify({ email: email, password: password }),
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      let parsedResp = await response.json();
+      console.log(parsedResp);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container id="global-Body" className="mb-4 loginPage">
       <Row>
@@ -16,39 +59,113 @@ function Login() {
         </Col>
         <Col className="mt-3" xs={{ span: "6", offset: "3" }}>
           <div className="d-flex mb-3 justify-content-between align-items-center">
-            <h4 className="m-0">Sign In</h4>{" "}
-            <a href={process.env.REACT_APP_BE_URL + `/users/3rdParty/google`}>
-              <Button className="signup-btn google">
-                <FcGoogle />
-              </Button>
-            </a>
+            <h4 className="m-0">Sign {register ? "Up" : "In"}</h4>
+            <OverlayTrigger
+              key="top"
+              placement="top"
+              overlay={
+                <Tooltip id="registerToolTip">Login with Google</Tooltip>
+              }
+            >
+              <a href={process.env.REACT_APP_BE_URL + `/users/3rdParty/google`}>
+                <Button className="signup-btn google">
+                  <FcGoogle />
+                </Button>
+              </a>
+            </OverlayTrigger>
           </div>
-          <Form>
-            <Form.Group>
-              <Form.Label htmlFor="emailInput">Email:</Form.Label>
-              <Form.Control id="emailInput" />
-            </Form.Group>
-            <Form.Row>
-              <Col xs={6}>
-                <Form.Group>
-                  <Form.Label htmlFor="passwordInput">Password:</Form.Label>
-                  <Form.Control id="passwordInput" />
-                </Form.Group>
-              </Col>
-              <Col xs={6}>
-                <Form.Group>
-                  <Form.Label htmlFor="password2Input">
-                    Confirm Password:
-                  </Form.Label>
-                  <Form.Control id="password2Input" />
-                </Form.Group>
-              </Col>
-            </Form.Row>
-            <Form.Group>
-              <Form.Label htmlFor="nameInput">Name:</Form.Label>
-              <Form.Control id="nameInput" />
-            </Form.Group>
-          </Form>
+          {register ? (
+            <Form
+              noValidate
+              autoComplete="off"
+              validated={validated}
+              onSubmit={(e) => handleSubmit(e)}
+              className="d-flex flex-column justify-content-end"
+            >
+              <Form.Group>
+                <Form.Label htmlFor="emailInput">Email:</Form.Label>
+                <Form.Control id="emailInput" />
+              </Form.Group>
+              <Form.Row>
+                <Col xs={6}>
+                  <Form.Group>
+                    <Form.Label htmlFor="passwordInput">Password:</Form.Label>
+                    <Form.Control id="passwordInput" />
+                  </Form.Group>
+                </Col>
+                <Col xs={6}>
+                  <Form.Group>
+                    <Form.Label htmlFor="password2Input">
+                      Confirm Password:
+                    </Form.Label>
+                    <Form.Control id="password2Input" />
+                  </Form.Group>
+                </Col>
+              </Form.Row>
+              <Form.Group>
+                <Form.Label htmlFor="nameInput">Name:</Form.Label>
+                <Form.Control id="nameInput" />
+              </Form.Group>
+              <Button className="signup-btn ml-auto" type="submit">
+                <AiOutlineLogin color="black" />
+              </Button>
+            </Form>
+          ) : (
+            <Form
+              noValidate
+              autoComplete="off"
+              validated={validated}
+              onSubmit={(e) => handleSubmit(e)}
+              className="d-flex flex-column justify-content-end"
+            >
+              <Form.Group>
+                <Form.Label htmlFor="emailInput">Email:</Form.Label>
+                <Form.Control
+                  required
+                  id="emailInput"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.currentTarget.value)}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label htmlFor="passwordInput">Password:</Form.Label>
+                <Form.Control
+                  required
+                  id="passwordInput"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                />
+              </Form.Group>
+              <div className="d-flex">
+                <OverlayTrigger
+                  key="top"
+                  placement="top"
+                  overlay={<Tooltip id="registerToolTip">Register</Tooltip>}
+                >
+                  <Button
+                    className="signup-btn ml-auto"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setRegister(true);
+                    }}
+                  >
+                    <AiOutlineUserAdd color="black" />
+                  </Button>
+                </OverlayTrigger>
+                <OverlayTrigger
+                  key="top"
+                  placement="top"
+                  overlay={<Tooltip id="registerToolTip">Login</Tooltip>}
+                >
+                  <Button className="signup-btn ml-2" type="submit">
+                    <AiOutlineLogin color="black" />
+                  </Button>
+                </OverlayTrigger>
+              </div>
+            </Form>
+          )}
         </Col>
       </Row>
     </Container>
